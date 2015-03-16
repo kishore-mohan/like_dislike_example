fff1714bc489e66b1174fc7ef76481946c22a7b8
rails-geek <nappinnai.m@gmail.com>
2015-03-16 22:11:32 +0530

    initializ repository with devise and like_unlike gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LikeDislikeExample::Application.config.secret_token = 'cae8d93908cf1399f4a3cfd7ae6757ae7254341d32bf0d86df12eeb22b6c85dd80f6a99929cd17c7a17ddebde975642a39b74d5e8ae012ea8cb6e3754a8e4000'
